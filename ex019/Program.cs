f71a8af3c73b08ffb792dd779058d78de00d71a6
Natalya <h.h.smolnikova@mail.ru>
2022-07-09 23:36:33 +0500

    задача19

---
+++ ex019/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход пятизначное число и
+        //проверяет, являутся ли оно палиндромом
+
+        Console.WriteLine("Введите пятизначное число: ");
+        string? number = Console.ReadLine();
+        
+        if (number[0] == number[4] || number[1] == number[3])
+        {
+            Console.WriteLine($" -> да : {number}" );
+        }
+
+        else
+        {
+             Console.WriteLine($" -> нет : {number}" );
+        }
+            
+    }
+}
+}
