389dd44689c7d9ee2f78b6a4e34e896c02b3d725
Natalya <h.h.smolnikova@mail.ru>
2022-07-12 22:36:10 +0500

    задача 23

---
+++ ex023/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                PrintPow(i);
+            }            
+        }
+        public static void PrintPow(int n)
+        {
+            Console.WriteLine(n*n*n);
+        }
+    }
+}
